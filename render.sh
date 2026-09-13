47dd74fce504083846be406dafe6f4ccd2e9ac50
Andrew Andrade <andrew@andrewandrade.ca>
2026-09-13 11:35:51 -0400

    Generate combined Word tracker with embedded Pandoc

--- render.sh
+++ render.sh
@@ -36,7 +36,7 @@ build_teacher_marking() {
   quarto render assignments/combined-teacher-marking.qmd
   mv -f assignments/combined-teacher-marking.pdf dist/combined-teacher-marking.pdf
 
-  quarto render assignments/combined-teacher-marking-word.qmd
+  quarto pandoc assignments/combined-teacher-marking-word.md --to docx --output assignments/combined-teacher-marking.docx
   mv -f assignments/combined-teacher-marking.docx dist/combined-teacher-marking.docx
 }
 
